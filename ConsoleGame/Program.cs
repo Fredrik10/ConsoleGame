4940924cd2ca58c3b88661d5add5c4fd62f6f990
elev <elev@5CG62366CS>
2018-09-05 10:27:14 +0200

    Skapat spel struktur, men inget mer.

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -8,8 +8,38 @@ namespace ConsoleGame
 {
     class Program
     {
+        /// <summary>
+        /// //Initiera värden i början av programmet 
+        /// </summary>
+        static void Initialize ()
+        {
+
+        }
+        /// <summary>
+        /// Ritar ut på bildskärmen
+        /// </summary>
+        static void Draw()
+        {
+
+        }
+        /// <summary>
+        /// AInnehåller pogrammets logik
+        /// </summary>
+        static void Update()
+        {
+
+        }
         static void Main(string[] args)
         {
+            Initialize();
+            
+            while(true)
+            {
+                Update();
+                Draw();
+            }
+
+
         }
     }
 }
